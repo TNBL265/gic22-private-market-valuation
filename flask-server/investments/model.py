b556b263ee201365a97894699d39a704c2e92f02
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-09-16 13:50:02 +0800

    feat: integrate SQLite to investments module

--- flask-server/investments/model.py
+++ flask-server/investments/model.py
@@ -1,49 +1,43 @@
-from . import rds_db
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+rds_db = SQLAlchemy()
 
 
 class InvestmentData(rds_db.Model):
-    __tablename__: str = "share_data"
+    __tablename__: str = "investment_data"
     __table_args__ = {'extend_existing': True}
 
     id = rds_db.Column(rds_db.Integer, primary_key=True, autoincrement=True)
     date = rds_db.Column(rds_db.DateTime, nullable=False)
-    volume = rds_db.Column(rds_db.Float, nullable=False)
+    value = rds_db.Column(rds_db.Float, nullable=False)
     quantity = rds_db.Column(rds_db.Float, nullable=False)
 
-    def __init__(self, date, volume, quantity):
+    def __init__(self, date, value, quantity):
         self.date = date
-        self.volume = volume
+        self.value = value
         self.quantity = quantity
 
     def to_map_as_date_string(self):
         return {
             "date": self.date.strftime('%Y-%m-%d %H:%M:%S'),
-            "volume": self.volume,
+            "value": self.value,
             "quantity": self.quantity
         }
 
 
-def insert_investment_data(data_obj):
-    date: str = data_obj['date']
-    investment_data_entries = data_obj['data']
-
-    data_list = [InvestmentData(datetime.strptime(date, '%Y-%m-%d %H:%M:%S'), item['volume'], item['quantity'])
-                 for item in investment_data_entries]
-
-    rds_db.session.add_all(data_list)
+def insert_one_investment_data(data_obj):
+    data = data_obj['data']
+    data = InvestmentData(datetime.strptime(data['date'], '%Y-%m-%d %H:%M:%S'), data['value'], data['quantity'])
+    rds_db.session.add(data)
     rds_db.session.commit()
     rds_db.session.remove()
     rds_db.session.close()
-    return len(data_list)
 
 
 def get_investment_data(date: datetime):
     object_list = InvestmentData.query.filter(InvestmentData.date == date).all()
     data_list = [investment_data.to_map_as_date_string() for investment_data in object_list]
 
-    data_map = {
-        "date": date,
-        "data": data_list
-    }
+    data_map = {"data": data_list}
     return data_map
--- flask-server/investments/wsgi.py
+++ flask-server/investments/wsgi.py
@@ -1,30 +1,17 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 from controller import rds_blueprint
+from model import rds_db
 
 
 rds_app = Flask(__name__)
 rds_app.config.from_object(Config)
 rds_app.register_blueprint(rds_blueprint)
-rds_db = SQLAlchemy(rds_app)
-
-
-@rds_app.cli.command("reset_db")
-def reset_db():
-    rds_db.drop_all()
-    rds_db.create_all()
-    rds_db.session.commit()
-    print("DB has been reset")
-
-
-@rds_app.cli.command("create_db")
-def create_db():
-    rds_db.create_all()
-    rds_db.session.commit()
-    print("DB is created")
+rds_db.init_app(rds_app)
 
 
 if __name__ == '__main__':
+    with rds_app.app_context():
+        rds_db.create_all()
     rds_app.run(host='0.0.0.0', port=5004, debug=False)
