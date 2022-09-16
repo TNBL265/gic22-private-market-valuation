0fc290a1744eb056b98b4968799d581767d12075
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-09-16 11:49:30 +0800

    chore: specify port for flask module

--- flask-server/market_valuations/wsgi.py
+++ flask-server/market_valuations/wsgi.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
-from .controller import redshift_blueprint
+from controller import redshift_blueprint
 
 
 redshift_app = Flask(__name__)
 redshift_app.register_blueprint(redshift_blueprint)
+
+if __name__ == '__main__':
+    redshift_app.run(host='0.0.0.0', port=5002, debug=False)
--- flask-server/transactions/wsgi.py
+++ flask-server/transactions/wsgi.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
-from .controller import dynamo_blueprint
+from controller import dynamo_blueprint
 
 
 dynamo_app = Flask(__name__)
 dynamo_app.register_blueprint(dynamo_blueprint)
+
+if __name__ == '__main__':
+    dynamo_app.run(host='0.0.0.0', port=5003, debug=False)
