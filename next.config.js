4ec938159f906c14278932129fdd7fad7eadd8a6
Randell Crapy <randell.crapy@gmail.com>
2022-09-18 01:25:52 +0800

    Added bulk upload feature for instruments, market value stuff same pattern can do tmr

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ module.exports = () => {
         source: "/api/market-values/:path*",
         destination: "http://127.0.0.1:5002/market-values/:path*",
       },
+      {
+        source: "/api/transactions/:path*",
+        destination: "http://127.0.0.1:5003/transactions/:path*",
+      },
     ];
   };
   return {
