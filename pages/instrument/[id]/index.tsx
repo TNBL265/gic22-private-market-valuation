6b467a5270b7187bad32629283a115212d3d5437
hjunleon <randell.crapy@gmail.com>
2022-09-17 18:30:55 +0800

    Better instrument id page

--- pages/instrument/[id]/index.tsx
+++ pages/instrument/[id]/index.tsx
@@ -1,8 +1,39 @@
 import { useRouter } from "next/router";
+import TextField from '@mui/material/TextField';
 import LeftNavbar from "../../../components/common/LeftNavbar/LeftNavbar";
 import InstrumentChart from "../../../components/Instruments/Instrument/InstrumentChart";
 
-const BlockClassName = "p-4 rounded-lg bg-white";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+
+const BlockClassName = "p-4 rounded-lg bg-white relative z-10";
+
+const InstrumentDetails = () => {
+  const Entry = ({label, value}: any) => {
+    return (
+      <div className="flex justify-between text-sm font-semibold my-2">
+        <div className="text-my-gray-2">{label}</div>
+        <div>{value}</div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <h2 className="text-2xl mb-2">Details</h2>
+      <Entry label="Country" value="USA"/>
+      <Entry label="Currency" value="USA"/>
+      <Entry label="Sector" value="USA"/>
+      <Entry label="Type" value="USA"/>
+      <Entry label="Tradeable" value="Yes"/>
+      <Entry label="Created At" value="Yes"/>
+      <Entry label="Modified At" value="Yes"/>
+      {/* <Entry label="Tradeable" value="Yes"/> */}
+      <div className="text-my-gray-2 text-sm font-semibold my-2">Notes</div>
+      <p>Private Equity; Financials; Tradable Instrument</p>
+    </>
+  );
+};
 
 const InstrumentPage = () => {
   const router = useRouter();
@@ -12,16 +43,60 @@ const InstrumentPage = () => {
   return (
     <div className="page__container flex relative ">
       {/* <div className=""> */}
-        <LeftNavbar />
+      <LeftNavbar />
       {/* </div> */}
-      <div className="page__body grow">
-        <div className="bg-zinc-800 rounded-t-2xl h-[20%]"></div>
-        <div className="p-2">
-        <div className={BlockClassName}>
-          <InstrumentChart />
+      <div className="page__body grow relative">
+        <div className="p-8 z-10 relative">
+          <div className="flex items-center my-6">
+            <h1 className="text-5xl text-white font-bold mr-2">
+              Armstrong - Jacobi
+            </h1>
+            <EditIcon className="text-white w-8 h-8 mr-2" />
+            <DeleteForeverIcon className="text-white w-8 h-8" />
+          </div>
+          <div className="flex items-center justify-between my-6">
+            <div className="p-4 bg-white rounded-2xl flex items-center grow mr-4">
+              <div className="">
+                <h4 className="text-2xl">PnL</h4>
+                <div className="text-my-green-1">+24%</div>
+              </div>
+            </div>
+
+            <div className="p-4 bg-white rounded-2xl flex items-center grow mx-4">
+              <div className="">
+                <h4 className="text-2xl">Portfolio Value</h4>
+                <div className="text-my-green-1">$35600</div>
+              </div>
+            </div>
+
+            <div className="p-4 bg-white rounded-2xl flex items-center grow mx-4">
+              <div className="">
+                <h4 className="text-2xl">Risk/Volatility</h4>
+                <div className="text-my-green-1">4/10</div>
+              </div>
+            </div>
+
+            <div className="p-4 bg-white rounded-2xl flex items-center grow ml-4">
+              <div className="">
+                <h4 className="text-2xl">Percentage Portfolio</h4>
+                <div className="text-my-green-1">10%</div>
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="grow mr-2">
+              <div className={BlockClassName}>
+                <InstrumentChart />
+              </div>
+            </div>
+            <div className="shrink-0 w-1/3 ml-2">
+              <div className={BlockClassName}>
+                <InstrumentDetails />
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-        
+        <div className="absolute top-0 left-0 w-full bg-zinc-800 rounded-tl-2xl h-[20%] z-0"></div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,41 +11,38 @@ module.exports = {
     fontFamily: {
       TTFirs: ["TTFirsNeue"],
     },
-    colors:{
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',  // !important
-      'my-blue':{
-        1: "#0032A1",
-        2: "#7096EA",
-        3: "#92B2F8",
-      },
-      'my-gray':{
-        1: "#393939",
-        2: "#AAA6A6",
-        3: "#6D6D6D",
-        4: "#F2F2F2",
-        5: "#F6F6F6",
-      },
-      'my-green':{
-        1:"#0DB14F",
-        2:"#53F88B"
-      },
-      'my-red':{
-        1:"#EC0039"
-      },
-      'orange':{
-        1:"#FFC700"
-      }
-    },
     extend: {
       boxShadow: {
         popUps: "0 0.0625rem 3.125rem 0 rgb(0 0 0 / 20%)",
       },
-      // colors: {
-      //   "gray-dark": "#141414",
-      //   "salmon-orange": "#FA5F3C",
-      // },
+
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff", // !important
+        "my-blue": {
+          1: "#0032A1",
+          2: "#7096EA",
+          3: "#92B2F8",
+        },
+        "my-gray": {
+          1: "#393939",
+          2: "#AAA6A6",
+          3: "#6D6D6D",
+          4: "#F2F2F2",
+          5: "#F6F6F6",
+        },
+        "my-green": {
+          1: "#0DB14F",
+          2: "#53F88B",
+        },
+        "my-red": {
+          1: "#EC0039",
+        },
+        orange: {
+          1: "#FFC700",
+        },
+      },
       zIndex: {
         100: "100",
         "-1": "-1",
@@ -54,7 +51,7 @@ module.exports = {
       },
       height: {
         "i-20": "calc(100% - 5rem)",
-        "i-16": "calc(100% - 4rem)"
+        "i-16": "calc(100% - 4rem)",
       },
       animation: {
         "slide-show": "slideee 75s linear infinite",
