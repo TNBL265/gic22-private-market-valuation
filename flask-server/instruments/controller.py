c689cd7ff0770f02cf5d16b596d27ccf86b500cf
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-09-17 12:42:52 +0800

    :sparkles: feat: POST and GET request for instruments

--- flask-server/instruments/controller.py
+++ flask-server/instruments/controller.py
@@ -1,8 +1,7 @@
 import json
 from flask import Blueprint, make_response, request
-from datetime import datetime
 
-from model import insert_one_instrument_data, get_instrument_data
+from model import *
 
 instruments_blueprint = Blueprint('instruments_api', __name__)
 
@@ -10,16 +9,11 @@ instruments_blueprint = Blueprint('instruments_api', __name__)
 @instruments_blueprint.route("/instruments", methods=['GET', 'POST'])
 def instrument_data_controller():
     if request.method == 'GET':
-        date: str = request.args.get('date')
-        if not date:
-            return make_response("Missing argument(s) or argument(s) is empty", 400)
-
-        data_list = get_instrument_data(datetime.strptime(date, '%Y-%m-%d %H:%M:%S'))
+        data_list = retrieve_list_of_instruments()
         return make_response(json.dumps(data_list), 200)
-
     elif request.method == 'POST':
         data_obj = json.loads(request.data)
-        insert_one_instrument_data(data_obj)
-        return make_response(json.dumps("Inserted"), 200)
+        data = create_an_instrument(data_obj)
+        return make_response(json.dumps(data), 200)
     else:
         return make_response(json.dumps("Request type not supported"), 400)
--- flask-server/instruments/model.py
+++ flask-server/instruments/model.py
@@ -1,43 +1,68 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 instruments_db = SQLAlchemy()
 
 
 class InstrumentData(instruments_db.Model):
     __tablename__: str = "instrument_data"
-    __table_args__ = {'extend_existing': True}
+    __table_args__ = {"tend_existing": True}
 
-    id = instruments_db.Column(instruments_db.Integer, primary_key=True, autoincrement=True)
-    date = instruments_db.Column(instruments_db.DateTime, nullable=False)
-    value = instruments_db.Column(instruments_db.Float, nullable=False)
-    quantity = instruments_db.Column(instruments_db.Float, nullable=False)
+    instrumentId = instruments_db.Column(instruments_db.Integer, primary_key=True, autoincrement=True)
+    instrumentName = instruments_db.Column(instruments_db.String, nullable=False)
+    instrumentType = instruments_db.Column(instruments_db.String, nullable=False)
+    country = instruments_db.Column(instruments_db.String, nullable=False)
+    sector = instruments_db.Column(instruments_db.String, nullable=False)
+    instrumentCurrency = instruments_db.Column(instruments_db.String, nullable=False)
+    isTradeable = instruments_db.Column(instruments_db.Boolean, nullable=False)
+    createdAt = instruments_db.Column(instruments_db.DateTime(timezone=True), server_default=func.now())
+    modifiedAt = instruments_db.Column(instruments_db.DateTime(timezone=True), onupdate=func.now())
+    notes = instruments_db.Column(instruments_db.String, nullable=True)
 
-    def __init__(self, date, value, quantity):
-        self.date = date
-        self.value = value
-        self.quantity = quantity
+    def __init__(self, instrumentName, instrumentType, country, sector, instrumentCurrency, isTradeable, notes):
+        self.instrumentName = instrumentName
+        self.instrumentType = instrumentType
+        self.country = country
+        self.sector = sector
+        self.instrumentCurrency = instrumentCurrency
+        self.isTradeable = isTradeable
+        self.notes = notes
 
     def to_map_as_date_string(self):
+        modifiedAt = self.modifiedAt.strftime("%Y-%m-%d %H:%M:%S") if self.modifiedAt else ""
         return {
-            "date": self.date.strftime('%Y-%m-%d %H:%M:%S'),
-            "value": self.value,
-            "quantity": self.quantity
+            "instrumentId": self.instrumentId,
+            "instrumentName": self.instrumentName,
+            "instrumentType": self.instrumentType,
+            "country": self.country,
+            "sector": self.sector,
+            "instrumentCurrency": self.instrumentCurrency,
+            "isTradable": self.isTradeable,
+            "createdAt": self.createdAt.strftime("%Y-%m-%d %H:%M:%S"),
+            "modifiedAt": modifiedAt,
+            "notes": self.notes
         }
 
 
-def insert_one_instrument_data(data_obj):
-    data = data_obj['data']
-    data = InstrumentData(datetime.strptime(data['date'], '%Y-%m-%d %H:%M:%S'), data['value'], data['quantity'])
+def create_an_instrument(data_obj):
+    instruments_db.session.expunge_all()
+    data = data_obj["data"]
+    data = InstrumentData(data["instrumentName"], data["instrumentType"], data["country"], data["sector"],
+                          data["instrumentCurrency"], bool(data["isTradable"]), data.get("notes", None))
     instruments_db.session.add(data)
     instruments_db.session.commit()
+    output = {
+        "instrumentId": data.instrumentId,
+        "createdAt": data.createdAt.strftime("%Y-%m-%d %H:%M:%S")
+    }
     instruments_db.session.remove()
     instruments_db.session.close()
+    return output
 
 
-def get_instrument_data(date: datetime):
-    object_list = InstrumentData.query.filter(InstrumentData.date == date).all()
-    data_list = [instrument_data.to_map_as_date_string() for instrument_data in object_list]
-
+def retrieve_list_of_instruments():
+    instrument_list = InstrumentData.query.all()
+    data_list = [instrument_data.to_map_as_date_string() for instrument_data in instrument_list]
     data_map = {"data": data_list}
     return data_map
