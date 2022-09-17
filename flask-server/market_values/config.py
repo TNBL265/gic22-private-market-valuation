f441e07c59d4c9601123523aaeeb1e15c9efb878
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-09-17 16:09:32 +0800

    :recycle: refactor flask backend

--- flask-server/market_values/config.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'gic.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
