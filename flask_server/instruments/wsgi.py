1af124ce58158f6eb7344c6d5ba17accedc1328b
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-09-17 22:44:30 +0800

    :memo: update port for flask services

--- flask_server/instruments/wsgi.py
+++ flask_server/instruments/wsgi.py
@@ -18,4 +18,4 @@ db.init_app(instruments_app)
 if __name__ == "__main__":
     with instruments_app.app_context():
         db.create_all()
-    instruments_app.run(host='0.0.0.0', port=5001, debug=False)
+    instruments_app.run(host='0.0.0.0', port=5000, debug=False)
